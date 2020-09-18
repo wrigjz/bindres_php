09db0edd82641e7aae82327e577e061645c788d4
Jon Wright <jwright.taipei@gmail.com>
2020-09-18 13:34:44 +0800

    Changes to make pylint happy

--- get_chain_from_pdb_archive.py
+++ get_chain_from_pdb_archive.py
@@ -27,29 +27,29 @@ LISTFILE = open(sys.argv[1], "r")
 
 # Process the pdb id chain id list file
 for LIST in LISTFILE:
-    found_chain = 0
+    FOUND_CHAIN = 0
     PDB, CHAIN, *junk = [x.strip() for x in LIST.split()]
     pdblc = PDB.lower()
     chainlc = CHAIN.lower()
     chainuc = CHAIN.upper()
-    tempin = "original.pdb"
+    TEMPIN = "original.pdb"
     middle = pdblc[1:3]
-    tempout = "input.pdb"
+    TEMPOUT = "input.pdb"
 #/home/programs/pdb_copy/pdb/ts/pdb1tsr.ent.gz
     tempcmd = "gunzip -c /home/programs/pdb_copy/pdb/" + middle + "/pdb" + pdblc + \
               ".ent.gz" + " >| " + "original.pdb"
     os.system(tempcmd)
 # loop over all the lines in the pdb file looking for what we want
-    INFILE = open(tempin, "r")
-    OUTFILE = open(tempout, "w")
+    INFILE = open(TEMPIN, "r")
+    OUTFILE = open(TEMPOUT, "w")
     ENDMDL = 0
     for LINE in INFILE:
-        if LINE[0:6] == "ENDMDL" and found_chain == 1: # We're found the chain and now ENDMDL
+        if LINE[0:6] == "ENDMDL" and FOUND_CHAIN == 1: # We're found the chain and now ENDMDL
             print("All done")
             break
         if LINE[0:4] == "ATOM" or LINE[0:3] == "TER":
             if LINE[21:22] == chainuc:  # We only want our chain
-                found_chain = 1         # we only want the first NMR model too
+                FOUND_CHAIN = 1         # we only want the first NMR model too
                 if LINE[16:17] == " ": # We only want the A or only atomic positions
                     OUTFILE.write(LINE)
                 if LINE[16:17] == "A":
